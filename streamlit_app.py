01c6f4cc5b5782b902c8f668e316535f5952287d
mjones102 <136463564+mjones102@users.noreply.github.com>
2023-07-04 17:26:53 +0100

    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,9 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get Fruit Foad List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list
+    my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
