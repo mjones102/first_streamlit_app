2dfa1d867bae9fba9d99b2e877135c2db74a94e2
mjones102 <136463564+mjones102@users.noreply.github.com>
2023-07-04 17:15:37 +0100

    cleaned up api section

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+
+#API SECTION HERE
 streamlit.header('Fruityvice Fruit Advice!')
-#this is now streamlit is used to get user input
-fruit_choice = streamlit.text_input('What fruit would you like information on?','Kiwi')
-streamlit.write('The selected fruit is ',fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#normalise for table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+    #this is now streamlit is used to get user input
+    fruit_choice = streamlit.text_input('What fruit would you like information on?','Kiwi')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information.')
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
