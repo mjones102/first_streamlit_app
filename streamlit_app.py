5e42ac19cfca0cb65018ccc90712f780566399f4
mjones102 <136463564+mjones102@users.noreply.github.com>
2023-07-04 14:38:09 +0100

    fixed dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 #normalise for table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
