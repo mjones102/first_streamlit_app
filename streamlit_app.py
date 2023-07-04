cd1d81d17b46349235e98ea23d40e6a83454a587
mjones102 <136463564+mjones102@users.noreply.github.com>
2023-07-04 17:09:44 +0100

    stop added

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+stremali.stop()
 
 import snowflake.connector
 
